23633b96f1bb7aa821cd17915ee99e4b0750a5ec
MuntoniFrancesca <francesca.muntoni@live.com>
2024-03-05 17:45:27 -0600

    added footer

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Nav from './components/Nav';
 import Main from './components/Main';
 import Menu from './components/Menu';
+import Footer from './components/Footer';
 import { BrowserRouter } from 'react-router-dom';
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
     <Nav/>
     <Main/>
     <Menu/>
+    <Footer/>
     </BrowserRouter>
     </>
   );
